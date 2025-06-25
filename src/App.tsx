cf8c90f87e8c93ea3a6c011703c684a784027369
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 21:41:14 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,47 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// MSAL Imports
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig";
 
+// Page Imports
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
-import PasswordResetPage from "./pages/PasswordResetPage";
-import RegistrationPage from "./pages/RegistrationPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
-
+const msalInstance = new PublicClientApplication(msalConfig);
 
 const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
+  <MsalProvider instance={msalInstance}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {/* 
+              LoginPage is now the public entry point. 
+              Protected routes like Dashboard are accessed via links in the Header, 
+              which will only render for authenticated users.
+            */}
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
 
-
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/password-reset" element={<PasswordResetPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
+            {/* 
+              The routes for registration and password-reset have been removed.
+              This functionality is now handled by the Microsoft identity platform.
+            */}
+            
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </MsalProvider>
 );
 
 export default App;
